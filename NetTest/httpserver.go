f5eafda98019c2a7cf4debd1d5c9f62698bf7186
知 未 <i237731947@outlook.com>
2018-04-10 18:41:00 +0800

    测试

--- NetTest/httpserver.go
+++ NetTest/httpserver.go
@@ -7,16 +7,18 @@ import (
 )
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Inside HelloServer handler")
-	fmt.Fprintf(w, "Hello,"+req.URL.Path[1:])
+	fmt.Fprintf(w, "Hello")
 }
 func PostServer(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Inside PostServer handler")
-	fmt.Fprintf(w, "Post,"+req.URL.Path[1:])
+	fmt.Fprintf(w, "Post")
+}
+func DefaultServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Default")
 }
 func main() {
 	http.HandleFunc("/Hello", HelloServer)
 	http.HandleFunc("/Post", PostServer)
+	http.HandleFunc("/", DefaultServer)
 	fmt.Println("Server start...")
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
