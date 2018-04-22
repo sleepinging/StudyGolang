5be1ab58cf0b0a9d4571908534d2b182b308d2d1
知 未 <i237731947@outlook.com>
2018-04-22 13:37:28 +0800

    测试

---
+++ Test/调用其它语言/C.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+#include <windows.h>
+int myadd(int a,int b){
+	return a+b;
+}
+void msgbox(wchar_t* c,wchar_t* t){
+	MessageBoxW(0,c,t,0);
+}
+*/
+import "C"
+import (
+	"fmt"
+	"twt/mystr"
+	"unsafe"
+)
+
+func msgbox(text, title string) {
+	C.msgbox((*C.wchar_t)(unsafe.Pointer(mystr.Str2uft16ptr(text))),
+		(*C.wchar_t)(unsafe.Pointer(mystr.Str2uft16ptr(title))))
+}
+
+func main() {
+	msgbox("内容", "测试")
+	r := C.myadd(1, 2)
+	fmt.Println(r)
+}
