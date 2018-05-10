cc4fc698d3b51cde5589ead8a41d78bb56a01a08
知 未 <i237731947@outlook.com>
2018-05-10 20:17:34 +0800

    静态网页服务器

--- NetTest/静态网页服务器/StaticWebServer.go
+++ NetTest/静态网页服务器/StaticWebServer.go
@@ -5,10 +5,22 @@ import (
 	"fmt"
 )
 
-func LoginHandler(w http.ResponseWriter, req *http.Request) {
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	name, ok := r.Form["UserName"]
+	pwd, ok := r.Form["Password"]
+	if !ok {
+		fmt.Fprintf(w, "请先登录")
+		return
+	}
+	fmt.Println(name, pwd)
 	w.WriteHeader(200)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "登录成功") //这个写入到w的是输出到客户端的
+	if name[0] == pwd[0] {
+		fmt.Fprintf(w, "登录成功")
+	} else {
+		fmt.Fprintf(w, "登录失败")
+	}
 }
 
 func main() {
