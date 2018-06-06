10b65e1a2381e47500385af22102ceea9264e8aa
知 未 <i237731947@outlook.com>
2018-06-06 14:15:43 +0800

    添加用户权限控制

--- 2018DMT/control/Permission/user.go
+++ 2018DMT/control/Permission/user.go
@@ -1,20 +1,56 @@
 package Permission
 
-import "net/http"
+import (
+	"net/http"
+	"../../dao"
+	"../../global"
+)
 
 //添加用户的权限检查
 func AddUser(w http.ResponseWriter, r *http.Request) (f bool, err error) {
-
+	uid, err := GetUserIdByCookie(w, r)
+	if err != nil {
+		return
+	}
+	tp := dao.GetUserType(uid)
+	if tp == 5 {
+		f = true
+		return
+	}
+	err = global.NoPermission
 	return
 }
 
 //修改用户的权限检查
-func UpDateUser(w http.ResponseWriter, r *http.Request) (f bool, err error) {
-
+func UpDateUser(id int, w http.ResponseWriter, r *http.Request) (f bool, err error) {
+	uid, err := GetUserIdByCookie(w, r)
+	if err != nil {
+		return
+	}
+	if uid == id {
+		f = true
+		return
+	}
+	tp := dao.GetUserType(uid)
+	if tp == 5 {
+		f = true
+		return
+	}
+	err = global.NoPermission
 	return
 }
 
 //删除用户的权限检查
-func DeleteUser(w http.ResponseWriter, r *http.Request) (f bool, err error) {
+func DeleteUser(id int, w http.ResponseWriter, r *http.Request) (f bool, err error) {
+	uid, err := GetUserIdByCookie(w, r)
+	if err != nil {
+		return
+	}
+	tp := dao.GetUserType(uid)
+	if tp == 5 {
+		f = true
+		return
+	}
+	err = global.NoPermission
 	return
 }
